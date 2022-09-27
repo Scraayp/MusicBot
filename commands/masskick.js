f08a5a60c519a278f153fa60ee721acb2c397a10
Scraayp <scraayp@gmail.com>
2022-09-27 14:04:20 +0200

    Start
    Signed-off-by: Scraayp <scraayp@gmail.com>

---
+++ commands/masskick.js
@@ -0,0 +1,91 @@
+const CaseModel = require('../db/Case.js');
+const { createCaseID} = require('../util/CaseGenerator.js')
+const {ApplicationCommandOptionType} = require('discord.js')
+
+module.exports = {
+    name: "masskick",
+    description: "Kick multiple people from the guild.",
+    permissions: "0x0000000000000800",
+    options: [
+        {
+            name: "role",
+            description: "Role you want to kick, if none provided. It will kick everyone with no role.",
+            type: ApplicationCommandOptionType.Role,
+            required: false,
+        },
+        {
+            name: "reason",
+            description: "The reason for the kick",
+            type: ApplicationCommandOptionType.String,
+            required: true
+        },
+    ],
+    run: async (client, interaction) => {
+      // Checks if the user has Administrator permission
+      if(!interaction.member.permissions.has("Administrator")) return interaction.reply({content:`You're not allowed to use this command.`, ephemeral: true});
+      
+      // Gets the user and reason variable
+      const role = interaction.options.getRole('role');
+      const reason = interaction.options.getString('reason');
+
+      
+
+      // Checks if the user is kickable!
+      if(user.id === client.user.id 
+      || user.id === interaction.member.id 
+      || user.id === interaction.guild.ownerId){
+        interaction.reply({content:`I'm not allowed to kick that user.`, ephemeral: true});
+        return;
+      }
+
+      // Setup caseID variable
+      let caseID = await createCaseID();
+
+      // Setup date variable
+      let caseFullDate = new Date();
+      let caseHours = await caseFullDate.getHours();
+      let caseMinutes = await caseFullDate.getMinutes();
+      let caseDay = await caseFullDate.getDate();
+      let caseMonth = await caseFullDate.getMonth() + 1;
+      let caseYear = await caseFullDate.getFullYear();
+      let caseDate = caseHours+":"+caseMinutes + " | " + caseDay+"/"+caseMonth+"/"+caseYear;
+
+      // Creates a case in the database
+      const Case = new CaseModel({
+        _id: caseID,
+        caseID: caseID,
+        caseGuild: interaction.guild.id,
+        userID: user.id,
+        caseMod: interaction.member.id,
+        caseAction: "Kick",
+        caseReason: reason,
+        caseDate: caseDate,
+      });
+      
+      // Don't forget to save the case!
+      Case.save();
+
+
+      try {
+        // Fetch the member from discord
+        let userMember = await interaction.guild.members.fetch(user.id);
+        
+        // Kick the fetched member snowflake
+        userMember.kick(`Case ID: ${Case.caseID} | Case Mod: ${interaction.member.user.tag}`).catch(err => {
+          if(err){
+              interaction.reply({content:`Something went wrong. Please contact the bot administrator!`, ephemeral: true})
+              return console.error(err);
+          }
+        })
+      }catch (e) {
+        if (e){
+          interaction.reply({content:`Something went wrong. Please contact the bot administrator!`, ephemeral: true})
+          return console.error(e);
+        }
+      }
+
+      // Sends a message back
+      interaction.reply({content:`**${user.tag}** *(${user.id})* has been kicked! Case ID: ${Case.caseID}`, ephemeral: true})
+    },
+  };
+  
